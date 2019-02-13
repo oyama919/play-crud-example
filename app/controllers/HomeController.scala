7ec4154a13c7ab8e2cf7fa776996d066ea25d3a9
oyama919 <oyama919@gmail.com>
2019-02-13 13:13:10 +0900

    add form to index page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,6 +7,20 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index() = Action {
-    Ok(views.html.index())
+    Ok(views.html.index(
+      "this is form message",
+    ))
+  }
+
+
+  def form() = Action { request =>
+    val form:Option[Map[String,Seq[String]]] =
+      request.body.asFormUrlEncoded
+    val param:Map[String,Seq[String]] = form.getOrElse(Map())
+    val name:String = param.get("name").get(0)
+    val password:String = param.get("pass").get(0)
+    Ok(views.html.index(
+      "name:" + name + ", password:" + password
+    ))
   }
 }
