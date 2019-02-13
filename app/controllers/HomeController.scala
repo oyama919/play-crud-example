401a6ae0983c9b8779b09d19e40d5edbee647764
oyama919 <oyama919@gmail.com>
2019-02-13 15:20:52 +0900

    create add user page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,18 +1,17 @@
 package controllers
 
-
-import java.sql._
-
-import javax.inject._
 import play.api.db.Database
 import play.api.mvc._
+import controllers.PersonForm._
+import java.sql._
+import javax.inject._
 
 
 @Singleton
 class HomeController @Inject()(db: Database, cc: MessagesControllerComponents)
   extends MessagesAbstractController(cc) {
 
-  def index() = Action {implicit request =>
+  def index() = Action { implicit request =>
     var msg = "database record:<br><ul>"
     try {
       db.withConnection { conn =>
@@ -30,5 +29,34 @@ class HomeController @Inject()(db: Database, cc: MessagesControllerComponents)
     Ok(views.html.index(
       msg
     ))
+  }
+
+  def add() = Action {implicit request =>
+    Ok(views.html.add(
+      "フォームを記入して下さい。",
+      form
+    ))
+  }
+
+  def create() = Action { implicit request =>
+    val formdata = form.bindFromRequest
+    val data = formdata.get
+    try
+      db.withConnection { conn =>
+        val ps = conn.prepareStatement(
+          "insert into people values (default, ?, ?, ?)")
+        ps.setString(1, data.name)
+        ps.setString(2, data.mail)
+        ps.setString(3, data.tel)
+        ps.executeUpdate
+      }
+    catch {
+      case e: SQLException =>
+        Ok(views.html.add(
+          "フォームに入力して下さい。",
+          form
+        ))
+    }
+    Redirect(routes.HomeController.index)
   }
 }
--- build.sbt
+++ build.sbt
@@ -7,11 +7,10 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.12.8"
 
-libraryDependencies += Seq(
+libraryDependencies ++= Seq(
   guice,
   "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.1" % Test,
   "com.h2database" % "h2" % "1.4.197",
   evolutions,
-  jdbc,
-  cache
+  jdbc
 )
