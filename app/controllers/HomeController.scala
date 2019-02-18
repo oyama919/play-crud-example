f83a11c8be5ff2b79a669c4b31cdeef136593517
oyama919 <oyama919@gmail.com>
2019-02-18 22:42:55 +0900

    modified index page and reset settings other pages code

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,144 +1,24 @@
 package controllers
 
-import play.api.db.Database
-import play.api.mvc._
-import controllers.PersonForm._
-import java.sql._
+
 import javax.inject._
+import models._
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
 
 
 @Singleton
-class HomeController @Inject()(db: Database, cc: MessagesControllerComponents)
+class HomeController @Inject()(repository: PersonRepository,
+  cc: MessagesControllerComponents)(implicit ec: ExecutionContext)
   extends MessagesAbstractController(cc) {
 
-  def index() = Action { implicit request =>
-    var msg = "database record:<br><ul>"
-    try {
-      db.withConnection { conn =>
-        val stmt = conn.createStatement
-        val rs = stmt.executeQuery("SELECT * from people")
-        while (rs.next) {
-          msg += "<li>" + rs.getInt("id") + ":" + rs.getString("name") + "</li>"
-        }
-        msg += "</ul>"
-      }
-    } catch {
-      case e:SQLException =>
-        msg = "<li>no record...</li>"
+  def index() = Action.async {implicit request =>
+    repository.list().map { people =>
+      Ok(views.html.index(
+        "People Data.", people
+      ))
     }
-    Ok(views.html.index(
-      msg
-    ))
   }
 
-  def add() = Action {implicit request =>
-    Ok(views.html.add(
-      "フォームを記入して下さい。",
-      form
-    ))
-  }
-
-  def create() = Action { implicit request =>
-    val formdata = form.bindFromRequest
-    val data = formdata.get
-    try
-      db.withConnection { conn =>
-        val ps = conn.prepareStatement(
-          "insert into people values (default, ?, ?, ?)")
-        ps.setString(1, data.name)
-        ps.setString(2, data.mail)
-        ps.setString(3, data.tel)
-        ps.executeUpdate
-      }
-    catch {
-      case e: SQLException =>
-        Ok(views.html.add(
-          "フォームに入力して下さい。",
-          form
-        ))
-    }
-    Redirect(routes.HomeController.index)
-  }
-
-  def edit(id:Int) = Action {implicit request =>
-    var formdata = form.bindFromRequest
-    try {
-      db.withConnection { conn =>
-        val stmt = conn.createStatement
-        val rs = stmt.executeQuery("select * from people where id=" + id)
-        rs.next
-        val name = rs.getString("name")
-        val mail = rs.getString("mail")
-        val tel = rs.getString("tel")
-        val data = Data(name, mail, tel)
-        formdata = form.fill(data)
-      }
-    } catch {
-      case e:SQLException =>
-        Redirect(routes.HomeController.index)
-    }
-    Ok(views.html.edit(
-      "フォームを編集して下さい。",
-      formdata, id
-    ))
-  }
-
-  def update(id:Int) = Action { implicit request =>
-    val formdata = form.bindFromRequest
-    val data = formdata.get
-    try
-      db.withConnection { conn =>
-        val ps = conn.prepareStatement("update people set name=?, mail=?, tel=? where id=?")
-        ps.setString(1, data.name)
-        ps.setString(2, data.mail)
-        ps.setString(3, data.tel)
-        ps.setInt(4, id)
-        ps.executeUpdate
-      }
-    catch {
-      case e: SQLException =>
-        Ok(views.html.add(
-          "フォームに入力して下さい。",
-          form
-        ))
-    }
-    Redirect(routes.HomeController.index)
-  }
-
-  def delete(id:Int) = Action {implicit request =>
-    var pdata:Data = null;
-    try {
-      db.withConnection { conn =>
-        val stmt = conn.createStatement
-        val rs = stmt.executeQuery("select * from people where id=" + id)
-        rs.next
-        val name = rs.getString("name")
-        val mail = rs.getString("mail")
-        val tel = rs.getString("tel")
-        pdata = Data(name, mail, tel)
-      }
-    } catch {
-      case e:SQLException =>
-        Redirect(routes.HomeController.index)
-    }
-    Ok(views.html.delete(
-      "このレコードを削除します。",
-      pdata, id
-    ))
-  }
-
-
-  def remove(id:Int) = Action { implicit request =>
-    try
-      db.withConnection { conn =>
-        val ps = conn.prepareStatement("delete from people where id=?")
-        ps.setInt(1, id)
-        ps.executeUpdate
-      }
-    catch {
-      case e: SQLException =>
-        Redirect(routes.HomeController.index)
-    }
-    Redirect(routes.HomeController.index)
-  }
 }
--- build.sbt
+++ build.sbt
@@ -7,10 +7,8 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.12.8"
 
-libraryDependencies ++= Seq(
-  guice,
-  "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.1" % Test,
-  "com.h2database" % "h2" % "1.4.197",
-  evolutions,
-  jdbc
-)
+libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+libraryDependencies += "com.h2database" % "h2" % "1.4.197"
+libraryDependencies += "com.typesafe.play" %% "play-slick" % "4.0.0"
+libraryDependencies += "com.typesafe.play" %% "play-slick-evolutions" % "4.0.0"
